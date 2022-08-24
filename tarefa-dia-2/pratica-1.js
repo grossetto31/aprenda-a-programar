d08790252b0f2c40a56bc91c11792fdbfb9c01fe
Gabriel Rossetto <gabrielpayao@hotmail.com>
2022-08-24 13:24:42 -0300

    desafio opcional

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -10,4 +10,7 @@ console.log(profissao)
 console.log(idade)
 console.log(localSonho)
 
-
+console.log("Olá,",name,"! Somos aqui da agência de turismo Viagem dos Sonhos. \n" + 
+"Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para",localSonho,".\n" +
+"Confirma pra gente alguns dados? A sua idade é ",idade,", você é ",profissao,"e o seu email é ",email,"?\n" +
+"Ficamos no aguardo. Muito obrigado e boa viagem! :D")
