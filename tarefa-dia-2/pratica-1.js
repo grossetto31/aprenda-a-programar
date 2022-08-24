8e5b73941aa8f151e1429f0566f5792a8cb5af1d
Gabriel Rossetto <gabrielpayao@hotmail.com>
2022-08-24 13:07:44 -0300

    atualização pratica-1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,11 @@ let email = "gabrielpayao@hotmail.com"
 let profissao = "Analista de Operacoes"
 let idade = 27
 let localSonho = "Liverpool"
+
+console.log(name)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(localSonho)
+
+
