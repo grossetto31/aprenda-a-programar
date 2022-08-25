daabd3bc80764977415610fb22dc29139b94ce16
Gabriel Rossetto <gabrielpayao@hotmail.com>
2022-08-25 13:28:20 -0300

    dia-3

---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,7 @@
+let num = 16 
+let i = 1
+
+while (i <= num) {
+    console.log(i)
+    i += 2 
+}
---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,14 @@
+let num = 17
+
+let i = 2
+let ehPrimo = true
+
+i = 2 
+while (num % i != 0)
+    i++ 
+
+
+if (num == i) 
+    console.log(`Sim, o número ${num} é primo.`)
+else
+    console.log(`Não, o número ${num} não é primo.`)
